ff2b9e75725fdaaf347b1048943343e8b2d88049
William S. Rodriguez <williamsrz@outlook.com>
2017-01-22 19:23:42 -0200

    ☠️

---
+++ api/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persona.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Program>()
+                .Build();
+            
+            host.Run();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvcWithDefaultRoute();
+        }
+
+    }
+}
